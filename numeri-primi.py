9125e3adfc57a4b91140040820f65c9fdaa61e3c
gid97 <giupy37@live.it>
2014-03-08 10:16:33 +0100

    Update numeri-primi.py

--- numeri-primi.py
+++ numeri-primi.py
@@ -2,6 +2,8 @@ from __future__ import division
 a=input('scrivi qui fin quanto vuoi calcolare i numeri primi ')
 b=range(3,a+1,2)
 d=[]
+x=[]
+r=a+1
 for c in b:
     for g in b:
         if c/2==g:
@@ -17,9 +19,12 @@ b.insert(0,2)
 d=[]
 for y in b:
   for z in b:
-    for x in b:
-      if y*z==x:
-	d.append(x)
+     if y*z<=r:
+       x.append(y*z)
+for t in x:
+  for i in b:
+    if t==i:
+      d.append(i)
 for h in d:
   for j in b:
       if h==j:
